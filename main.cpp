ee26f00566e731dc12666e60ea049c348eecdbf1
emvi <wamarek@poczta.onet.pl>
2020-03-11 22:48:39 +0100

    prawdopodobna koncepcja optymalizacji

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
  *      Author: emvi
  */
 
+/* Przypuszczam, że podczas rozmowy optymalizacją która miałaby przyspieszyć algorytm
+ * było zgromadzenie danych w drzewie Trie.
+ * Musiałem niestety w tym celu użyć google-a. Nie przypominam sobie żebym kiedykolwiek
+ * wcześniej słyszał o drzewach Trie.
+ * Czy ten trop jest właściwy?
+ */
+
 #include "db.h"
 #include "cli.h"
 
